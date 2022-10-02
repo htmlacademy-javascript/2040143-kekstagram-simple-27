0398825941b795ee6fe5455ba280ee26ad7e29eb
Анна Смольянинова <anna.smln@mail.ru>
2022-10-02 14:43:49 +0300

    добавляет функции проверки длины строки и возврата случайного целого числа

---
+++ js/main.js
@@ -0,0 +1,24 @@
+//Функция, возвращающая случайное целое число из переданного диапазона включительно, аргументы - целые числа, они больше или равны 0.
+//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger
+
+function getRandomNumber(firstNumber, secondNumber) {
+  if (firstNumber >= 0 && firstNumber <= secondNumber && Number.isInteger(firstNumber) && Number.isInteger(secondNumber)) {
+    firstNumber = Math.ceil(firstNumber);
+    secondNumber = Math.floor(secondNumber);
+    return Math.floor(Math.random() * (secondNumber - firstNumber + 1)) + firstNumber;
+  }
+
+  return NaN;
+}
+
+getRandomNumber();
+
+function checkLineLength(line, maxLength) {
+  if (line.length <= maxLength) {
+    return true;
+  }
+  return false;
+}
+
+checkLineLength();
