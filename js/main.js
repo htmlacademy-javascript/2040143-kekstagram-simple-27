2f1659a5d6034a45b5920c85869728ff7429aad8
Анна Смольянинова <anna.smln@mail.ru>
2022-10-11 20:11:02 +0300

    модуль 4

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@
 //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger
 
+
+function checkLineLength(line, maxLength) {
+  return line.length <= maxLength;
+}
+
+checkLineLength();
+
 function getRandomNumber(firstNumber, secondNumber) {
   if (firstNumber >= 0 && firstNumber <= secondNumber && Number.isInteger(firstNumber) && Number.isInteger(secondNumber)) {
     firstNumber = Math.ceil(firstNumber);
@@ -14,8 +21,17 @@ function getRandomNumber(firstNumber, secondNumber) {
 
 getRandomNumber();
 
-function checkLineLength(line, maxLength) {
-  return line.length <= maxLength;
-}
 
-checkLineLength();
+const descriptPicture = (index) => ({
+  id: index,
+  url: `photos/${ index }.jpg`,
+  description: 'Эта фотография сделана одним из наших авторов',
+  likes: getRandomNumber(15, 200),
+  comments: getRandomNumber(0, 200),
+});
+
+descriptPicture();
+
+const userPictures = Array.from({length: 25}, (_, pictureIndex) => descriptPicture (pictureIndex + 1));
+
+userPictures();
