d9b52d73221653e93fc3e5753c0bcd017ebae039
Анна Смольянинова <anna.smln@mail.ru>
2022-11-02 20:57:40 +0300

    модуль 8 задание 1

--- js/main.js
+++ js/main.js
@@ -2,3 +2,5 @@ import {createThumbnails} from './create-thumbnails.js';
 import {createPictures} from './create-pictures.js';
 const pictures = createPictures();
 createThumbnails(pictures);
+import './open-close-form.js';
+import './validate-pristine.js';
---
+++ js/validate-pristine.js
@@ -0,0 +1,15 @@
+const userPictureForm = document.querySelector('.img-upload__form');
+
+
+const pristine = new Pristine(userPictureForm, {
+  classTo: 'img-upload__text',
+  errorTextParent: 'img-upload__text',
+  errorTextClass: 'img-upload__error-text'
+});
+
+userPictureForm.addEventListener('submit', (evt) => {
+  const isValid = pristine.validate();
+  if(!isValid) {
+    evt.preventDefault(evt);
+  }
+});
