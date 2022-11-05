6ccc0a874f4e617e85689877370fdf9c2476c7a1
Анна Смольянинова <anna.smln@mail.ru>
2022-11-05 14:53:00 +0300

    исправляет ошибки в 8 модуле

--- js/open-close-form.js
+++ js/open-close-form.js
@@ -1,44 +1,47 @@
 import {isEscapeKey, isEnterKey} from './utils.js';
 
+
 const uploadPicture = document.querySelector('#upload-file');
 const pictureComment = document.querySelector('.text__description');
 const redactorPicture = document.querySelector('.img-upload__overlay');
 const body = document.querySelector('body');
 const imageFormCloseButton = document.querySelector('.img-upload__cancel');
 
-const onPictureFormEscDown = (evt) => {
-  if (isEscapeKey(evt)) {
-    evt.preventDefault();
+export const initOpenCloseForm = function () {
+  const onPictureFormEscDown = (evt) => {
+    if (isEscapeKey(evt)) {
+      evt.preventDefault();
+      closePictureForm();
+    }
+  };
+
+  uploadPicture.addEventListener('change', () => {
+    openPictureForm();
+  });
+
+  imageFormCloseButton.addEventListener('click', () => {
     closePictureForm();
+  });
+
+  imageFormCloseButton.addEventListener('keydown', (evt) => {
+    if (isEnterKey(evt)) {
+      closePictureForm();
+    }
+  });
+
+  function openPictureForm () {
+    redactorPicture.classList.remove('hidden');
+    body.classList.add('modal-open');
+
+    document.addEventListener('keydown', onPictureFormEscDown);
+  }
+
+  function closePictureForm () {
+    redactorPicture.classList.add('hidden');
+    body.classList.remove('modal-open');
+    uploadPicture.value = '';
+    pictureComment.value = '';
+
+    document.removeEventListener('keydown', onPictureFormEscDown);
   }
 };
-
-uploadPicture.addEventListener('change', () => {
-  openPictureForm();
-});
-
-imageFormCloseButton.addEventListener('click', () => {
-  closePictureForm();
-});
-
-imageFormCloseButton.addEventListener('keydown', (evt) => {
-  if (isEnterKey(evt)) {
-    closePictureForm();
-  }
-});
-
-function openPictureForm () {
-  redactorPicture.classList.remove('hidden');
-  body.classList.add('modal-open');
-
-  document.addEventListener('keydown', onPictureFormEscDown);
-}
-
-function closePictureForm () {
-  redactorPicture.classList.add('hidden');
-  body.classList.remove('modal-open');
-  uploadPicture.value = '';
-  pictureComment.value = '';
-
-  document.removeEventListener('keydown', onPictureFormEscDown);
-}
